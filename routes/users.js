9e0271300d85d614e23a63d5517bf8008bbe5a35
Ryan Webster <ryan.h.webster@gmail.com>
2020-12-01 14:42:21 -0800

    added login get and post

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { decodeBase64 } = require('bcryptjs');
 var express = require('express');
 var router = express.Router();
 
@@ -5,5 +6,55 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+router.get('/log-in', csrfProtection, (req,res) => {
+  res.render('login', {
+    title: 'Login',
+    csrfToken: req.csrfToken(),
+  });
+});
+
+const loginValidators = [
+  check('emailAddress').exists({ checkFalsy: true }).withMessage('Please provide Email Address'),
+  check('password').exists({ checkFalsy: true }).withMessage('Please provide a valid Password'),
+]
+
+router.post(
+  '/log-in',
+  csrfProtection,
+  loginValidators,
+  asyncHandler(async (req, res) => {
+    const { emailAdress, password } = req.body;
+
+    let errors = [];
+    const validationErrors = validationResult(req);
+
+    if(validationErrors.isEmpty()) {
+      const user = await db.User.findOne({ where: {emailAdress} });
+
+      if (user !== null) {
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+        if (passwordMatch) {
+          loginUser(req, res, user);
+          return res.redirect('/');
+        }
+      }
+
+      errors.push('Invalid credentials. Please doublecheck email address and password');
+    } else {
+      errors.validationErrors.array().map((error) => error.msg);
+    }
+
+    res.render('log-in', {
+      title: 'Login',
+      emailAdress,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
+
 
 module.exports = router;

de47d9da36056c2e380da1f14187579fe42e74ac
Daniel Miller <dbmiller25@gmail.com>
2020-12-01 15:55:52 -0500

    Added taggedTask model and migration

--- db/migrations/20201201185105-create-tagged-task.js
+++ db/migrations/20201201185105-create-tagged-task.js
@@ -1,30 +1,40 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('TaggedTasks', {
+    return queryInterface.createTable("TaggedTasks", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       taskId: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Tasks",
+          key: "id",
+        },
       },
       tagId: {
-        type: Sequelize.INTEGER
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Tags",
+          key: "id",
+        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('TaggedTasks');
-  }
+    return queryInterface.dropTable("TaggedTasks");
+  },
 };

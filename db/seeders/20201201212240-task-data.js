3215512ca5d75b309a142df5aaff19ed63be51a0
tony <tony@suas.com>
2020-12-01 17:18:05 -0500

    add queries to seeders to pull correct id's

--- db/seeders/20201201212240-task-data.js
+++ db/seeders/20201201212240-task-data.js
@@ -1,7 +1,8 @@
 'use strict';
+const db = require('../models');
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -12,19 +13,22 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    const lists = await db.List.findAll({
+      order: [['id']]
+    });
     return queryInterface.bulkInsert('Tasks', [
       {
         name: 'Garbage',
-        userId: 6,
-        listId: 9,
+        userId: lists[0].userId,
+        listId: lists[0].id,
         // due: Date('2020-12-25'),
         completed: false, createdAt: new Date(), updatedAt: new Date()
       },
       {
         name: 'Dishes',
         notes: "There are a lot of dishes, please clean them",
-        userId: 7,
-        listId: 10,
+        userId: lists[1].userId,
+        listId: lists[1].id,
         // due: Date('2020-12-24'),
         completed: false, createdAt: new Date(), updatedAt: new Date()
       },

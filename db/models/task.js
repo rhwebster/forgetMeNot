bcfa04ca46e6cd8394649b68b4935f66fd40f95f
Ryan Webster <ryan.h.webster@gmail.com>
2020-12-01 13:03:20 -0800

    task model

--- db/models/task.js
+++ db/models/task.js
@@ -1,15 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
-    name: DataTypes.STRING,
-    notes: DataTypes.TEXT,
-    userId: DataTypes.INTEGER,
-    listId: DataTypes.INTEGER,
-    due: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING(80),
+      allowNull: false,
+    },
+    notes: {
+      type: DataTypes.TEXT,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    listId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    due: {
+      type: DataTypes.DATE,
+    },
     completed: DataTypes.BOOLEAN
   }, {});
   Task.associate = function(models) {
-    // associations can be defined here
+    Task.belongsTo(models.List, { foreignKey: 'listId'});
+    Task.belongsTo(models.User, { foreignKey: 'userId' });
+    Task.belongsToMany(models.Tag, { through: "TaggedTask", foreignKey: "taskId", otherKey: "tagId", as: "TasksWithTags"})
   };
   return Task;
 };

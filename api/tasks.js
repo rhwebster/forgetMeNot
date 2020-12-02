76b04227f0c9fc784ccc9f5d878566b7b52a05f1
tony <tony@suas.com>
2020-12-02 17:16:48 -0500

    make popup window to add tags or lists - to do next

--- api/tasks.js
+++ api/tasks.js
@@ -3,12 +3,21 @@ const router = express.Router();
 const { Task, User, List } = require("../db/models");
 const { asyncHandler } = require("../routes/utils");
 
+router.get(
+  "/tasks",
+  asyncHandler(async (req, res) => {
+    const userId = req.session.auth.userId;
+    const list = await List.findOne({ where: { userId, name: "Inbox" } });
+    const tasks = await Task.findAll({ where: { userId, listId: list.id } });
+    res.json({ tasks });
+  })
+);
+
 router.post(
   "/tasks",
   asyncHandler(async (req, res) => {
-    console.log("HEY", req.body);
     const { name } = req.body;
-    console.log(name);
+
     const userId = req.session.auth.userId;
     const list = await List.findOne({ where: { userId, name: "Inbox" } });
     const task = await Task.create({
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,30 +1,41 @@
-// const asyncHandler = (handler) => (req, res, next) =>
-//   handler(req, res, next).catch(next);
+window.addEventListener("DOMContentLoaded", async (event) => {
+  const taskContainer = document.getElementById("list-of-tasks");
+  const addTaskButton = document.getElementById("add-task-button");
+  const taskField = document.getElementById("task-name");
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  const addTaskButton = document.getElementById("addTask");
-  const taskField = document.getElementById("taskName");
-  // addTaskButton.addEventListener("click", async (event) => {
-  //   const value = taskField.value;
-  //   const nameToSend = { name: value };
-  //   try {
-  //     const res = await fetch("http://localhost:8080/api/tasks", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(nameToSend),
-  //     });
-  //     let { tasks } = await res.json();
-  //     console.log(tasks);
-  //     const taskContainer = document.getElementById("taskContainer");
-  //     const newTask = tasks[tasks.length - 1].name;
-  //     const li = document.createElement("li");
-  //     li.innerHTML = newTask;
-  //     taskContainer.appendChild(li);
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // });
+  try {
+    const res = await fetch("/api/tasks");
+    let { tasks } = await res.json();
+    const taskHtml = [];
+    tasks.forEach((task) => {
+      let html = `<li>${task.name}</li>`;
+      taskHtml.push(html);
+    });
+    taskContainer.innerHTML = taskHtml.join("");
+  } catch (e) {
+    console.error(e);
+  }
 
+  addTaskButton.addEventListener("click", async (event) => {
+    const value = taskField.value;
+    const nameToSend = { name: value };
+    try {
+      const res = await fetch("/api/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(nameToSend),
+      });
+      let { tasks } = await res.json();
+      const taskHtml = [];
+      tasks.forEach((task) => {
+        let html = `<li>${task.name}</li>`;
+        taskHtml.push(html);
+      });
+      taskContainer.innerHTML = taskHtml.join("");
+    } catch (e) {
+      console.error(e);
+    }
+  });
   // Get the modal
   var modal = document.getElementById("myModal");
 
@@ -48,12 +59,5 @@ window.addEventListener("DOMContentLoaded", (event) => {
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  // window.onclick = function (event) {
-  //   if (event.target == modal) {
-  //     modal.style.display = "none";
-  //   }
-  // }
+  }  
 });

0e64a5b7070707fc3cf41724c372f1ce552a7380
tony <tony@suas.com>
2020-12-02 16:47:30 -0500

    move some more from master

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,26 +4,26 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const addTaskButton = document.getElementById("addTask");
   const taskField = document.getElementById("taskName");
-  // addTaskButton.addEventListener("click", async (event) => {
-  //   const value = taskField.value;
-  //   const nameToSend = { name: value };
-  //   try {
-  //     const res = await fetch("http://localhost:8080/api/tasks", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(nameToSend),
-  //     });
-  //     let { tasks } = await res.json();
-  //     console.log(tasks);
-  //     const taskContainer = document.getElementById("taskContainer");
-  //     const newTask = tasks[tasks.length - 1].name;
-  //     const li = document.createElement("li");
-  //     li.innerHTML = newTask;
-  //     taskContainer.appendChild(li);
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // });
+  addTaskButton.addEventListener("click", async (event) => {
+    const value = taskField.value;
+    const nameToSend = { name: value };
+    try {
+      const res = await fetch("http://localhost:8080/api/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(nameToSend),
+      });
+      let { tasks } = await res.json();
+      console.log(tasks);
+      const taskContainer = document.getElementById("taskContainer");
+      const newTask = tasks[tasks.length - 1].name;
+      const li = document.createElement("li");
+      li.innerHTML = newTask;
+      taskContainer.appendChild(li);
+    } catch (e) {
+      console.error(e);
+    }
+  });
 
   // Get the modal
   var modal = document.getElementById("myModal");

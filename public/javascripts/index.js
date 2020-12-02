2e4676202d45f4d9dfbe986c1297939472f04140
tony <tony@suas.com>
2020-12-02 16:59:32 -0500

    merge from homepage some elements

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,42 +4,44 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const addTaskButton = document.getElementById("addTask");
   const taskField = document.getElementById("taskName");
-  addTaskButton.addEventListener("click", async (event) => {
-    const value = taskField.value;
-    const nameToSend = { name: value };
-    try {
-      const res = await fetch("http://localhost:8080/api/tasks", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(nameToSend),
-      });
-      let { tasks } = await res.json();
-      console.log(tasks);
-      const taskContainer = document.getElementById("taskContainer");
-      const newTask = tasks[tasks.length - 1].name;
-      const li = document.createElement("li");
-      li.innerHTML = newTask;
-      taskContainer.appendChild(li);
-    } catch (e) {
-      console.error(e);
-    }
-  });
+  // addTaskButton.addEventListener("click", async (event) => {
+  //   const value = taskField.value;
+  //   const nameToSend = { name: value };
+  //   try {
+  //     const res = await fetch("http://localhost:8080/api/tasks", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(nameToSend),
+  //     });
+  //     let { tasks } = await res.json();
+  //     console.log(tasks);
+  //     const taskContainer = document.getElementById("taskContainer");
+  //     const newTask = tasks[tasks.length - 1].name;
+  //     const li = document.createElement("li");
+  //     li.innerHTML = newTask;
+  //     taskContainer.appendChild(li);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // });
 
   // Get the modal
   var modal = document.getElementById("myModal");
 
   // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-  var btn1 = document.getElementById("myBtn1");
+  var addTagBtn = document.getElementById("addTagBtn");
+  var addListBtn = document.getElementById("addListBtn");
+
+  console.log(addTagBtn);
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks the button, open the modal 
-  btn.onclick = function () {
+  addTagBtn.onclick = function () {
     modal.style.display = "block";
   }
-  btn1.onclick = function () {
+  addListBtn.onclick = function () {
     modal.style.display = "block";
   }
 

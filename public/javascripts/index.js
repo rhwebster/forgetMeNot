0298349a821e2ab9d295ba8b038f0052620894ee
tony <tony@suas.com>
2020-12-02 19:08:54 -0500

    add and remove tag AJAX-ly

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,6 +3,7 @@ window.addEventListener("DOMContentLoaded", async (event) => {
   const taskContainer = document.getElementById("list-of-tasks");
   const addTaskButton = document.getElementById("add-task-button");
   const taskField = document.getElementById("task-name");
+  const tagContainer = document.getElementById("list-of-tags-div");
 
   try {
     const res = await fetch("/api/tasks");
@@ -38,16 +39,60 @@ window.addEventListener("DOMContentLoaded", async (event) => {
     }
   });
   // Get the modal
-  var modal = document.getElementById("myModal");
+  const modal = document.getElementById("myModal");
 
   // Get the button that opens the modal
-  var addTagBtn = document.getElementById("addTagBtn");
-  var addListBtn = document.getElementById("addListBtn");
+  const addTagBtn = document.getElementById("addTagBtn");
+  const addListBtn = document.getElementById("addListBtn");
 
-  console.log(addTagBtn);
+  const btn = document.getElementById('addTag');
+
+  btn.addEventListener('click', async event => {
+    event.preventDefault();
+    const inputName = document.getElementById('inputName');    
+    const value = inputName.value;
+    const nameToSend = { name: value };    
+    try{
+      const res = await fetch('/api/tags', {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(nameToSend),
+      });
+      let { tags } = await res.json();
+      console.log(tags);
+      const tagHtml = [];
+      tags.forEach((tag) => {
+        let html = `<li id="li-${tag.name}">${tag.name} <button id="btn-${tag.name}">X</button></li>`;
+        tagHtml.push(html);
+      });
+      tagContainer.innerHTML = tagHtml.join("");      
+      tags.forEach((tag) => {
+        document.getElementById(`btn-${tag.name}`)
+          .addEventListener('click', async event=>{
+            event.preventDefault();
+            try{
+              const res = await fetch(`/api/tags/${tag.name}`, {
+                method: 'DELETE',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({name: tag.name}),   
+              });
+              let {name} = await res.json();
+              console.log("json back", name);
+              const li = document.getElementById(`li-${name}`);
+              tagContainer.removeChild(li);
+            } catch(e){
+
+            }
+          });
+      });
+
+    } catch(e){
+
+    }
+  });
 
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  const span = document.getElementsByClassName("close")[0];
 
   // When the user clicks the button, open the modal 
   addTagBtn.onclick = function () {
